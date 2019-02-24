94fe6d956fd0cf2e22b5112f95fe5c5c2ee2696f
zhuango <zhuangliu1992@mail.dlut.edu.cn>
2019-02-24 17:17:52 +0800

    package

--- golangLearning/ProgramStructure/main.go
+++ golangLearning/ProgramStructure/main.go
@@ -3,9 +3,13 @@ package main
 // /usr/lib/go-1.8/src/tempconv0.go (from $GOROOT)
 // /home/laboratory/go/src/tempconv0.go (from $GOPATH)
 import (
-	"tempconv"
+	"fmt"
+	//"tempconv"
+	"./popcount"
 )
 
 func main() {
-	tempconv.CRoF(1.23)
+	//tempconv.CRoF(1.23)
+	result := popcount.PopCountWithLoop(1000000)
+	fmt.Println(result)
 }
